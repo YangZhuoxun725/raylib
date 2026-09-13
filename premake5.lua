fa90d6f4d02533002724b22c859294e9b522f922
YangZhuoxun725 <yangzhuoxun1234@gmail.com>
2026-09-13 16:40:09 +0800

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+project "raylib"
+  kind "StaticLib"
+  language "C"
+  cdialect "C99"
+  staticruntime "On"
+
+  files
+  {
+    "src/raylib.h",
+    "src/rcamera.h",
+    "src/raymath.h",
+    "src/rlgl.h"
+    "src/rgestures.h",
+    "src/rlights.h",
+
+    "src/raylib.c",
+    "src/rcore.c",
+    "src/raudio.c",
+    "src/rmodels.c",
+    "src/rshapes.c",
+    "src/rtext.c",
+    "src/rtextures.c",
+    "src/utils.c"
+  }
+
+  includedirs
+  {
+    "src",
+    "src/external",
+    "src/external/glfw/include"
+  }
+
+  defines
+  {
+    "PLATFORM_DESKTOP",        
+    "GRAPHICS_API_OPENGL_33",
+    "_CRT_SECURE_NO_WARNINGS"
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+
+    links
+    {
+      "winmm",
+      "gdi32",
+      "opengl32"
+    }
+
+  filter "configurations:Debug"
+    symbols "On"
+    optimize "Debug"
+
+  filter "configurations:Release"
+    symbols "Off"
+    optimize "On"
+
+  filter "configurations:Dist"
+    symbols "Off"
+    optimize "Speed"
